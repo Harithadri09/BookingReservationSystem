6d4c9d8b23b262d3e0291ab78ed8b4c560e13a38
amirulyus <amirulyus@gmail.com>
2023-08-09 10:06:49 +0800

    update control register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,38 +17,29 @@ class Auth extends CI_Controller
 		$this->load->view('templates/auth_footer');
 	} 
 
-
-	public function registration()
-
-	{
-		$this->form_validation->set_rules('name', 'Name', 'required|trim');
-		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
-		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
-			'matches' => 'password dont match!',
-			'min_lenght' => 'pasword too short'
-		]);
-		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+	public function register() {
+        $this->form_validation->set_rules('firstname','First Name','trim|required');
+        $this->form_validation->set_rules('lastname','Last Name','trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|md5');
+        $this->form_validation->set_rules('cpassword', 'Confirm Password', 'required|matches[password]|md5');
 
 
-		if ($this->form_validation->run() == false) {
-			$data['title'] = 'User Registration';
-			$this->load->view('templates/auth_header', $data);
-			$this->load->view('auth/registration');
-			$this->load->view('templates/auth_footer');
-		} else {
-		$data = [
-			'name'=> $this->input->post('name'),
-			'email'=> $this->input->post('email'),
-			'image'=> 'default.jpg',
-			'password'=> password_hash($this->input->post('password'),
-			PASSWORD_DEFAULT),
-			'role_id'=> 2,
-			'is_active'=> 1,
-			'date_created'=> time()
+        $data['title'] = 'Register';
 
-		];
-			$this->db->insert('user',$data);
-			redirect('auth');
-		}		
-	}
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('users/register');
+            $this->load->view('templates/footer');
+
+        } else {
+            if ($this->user_model->set_user()) {
+                $this->session->set_flashdata('msg_success', 'Registration Successful!');
+                redirect('users/login');
+            } else {
+                $this->session->set_flashdata('msg_error', 'Error! Please try again later.');
+                redirect('users/register');
+            }
+        }
+    }
 } 
